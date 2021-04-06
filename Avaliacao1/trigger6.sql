efb2220a860ebde18dea7f8d2e3a8f9855bc1c0c
renanbaqui <46902597+renanbaqui@users.noreply.github.com>
2021-04-06 15:05:15 -0300

    Update trigger6.sql

--- Avaliacao1/trigger6.sql
+++ Avaliacao1/trigger6.sql
@@ -3,11 +3,11 @@
 -- localidades: cria uma nova tupla em circuitos, que vem pré-preenchida
 -- -------------------------------------------------------------------------
 
-CREATE OR REPLACE FUNCTION fc_adiciona_circuito2()
+CREATE OR REPLACE FUNCTION avaliacao1.fc_adiciona_circuito2()
 
 RETURNS trigger AS $$
 BEGIN
-	INSERT INTO circuitos (operadora, designacao, nome, mnemonico, 
+	INSERT INTO avaliacao1.circuitos (operadora, designacao, nome, mnemonico, 
 		ponta_a, ponta_b)
 	VALUES ('Adicionar Operadora', 'XXXXXXXXX00000', 'Adicionar Novo Nome', 
 		'XXX0', 99, 99);
@@ -16,9 +16,5 @@ BEGIN
 $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER nova_localidade AFTER INSERT OR UPDATE
-ON localidades
-	FOR EACH ROW EXECUTE PROCEDURE fc_adiciona_circuito2();
-	
-	
-	
-	
+ON avaliacao1.localidades
+	FOR EACH ROW EXECUTE PROCEDURE avaliacao1.fc_adiciona_circuito2();	
